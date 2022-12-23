f5845ad2ff9b2939830cdcbfbccc9533aee75f2d
anngelicafernanda <devanngelc@gmail.com>
2022-12-23 14:20:25 -0300

    Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import ImagenCripto from "./img/imagen-criptos.png";
 import Formulario from "./components/Formulario";
 import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -44,10 +45,13 @@ function App() {
 
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
  
   useEffect(() => {
   if (Object.keys(monedas).length>0) {
     const cotizarCripto = async ()=>{
+      setCargando(true)
+      setResultado({})
       const { moneda, criptomoneda } = monedas
       const url =`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
     
@@ -55,6 +59,7 @@ function App() {
       const resultado = await respuesta.json()
 
       setResultado(resultado.DISPLAY [criptomoneda] [moneda])
+      setCargando(false)
     }
     cotizarCripto()
   }
@@ -68,7 +73,7 @@ function App() {
         <Heading>Cotiza Criptomonedas al Instante</Heading> 
         <Formulario 
         setMonedas={setMonedas}/>
-
+        {cargando && <Spinner/>}
         {resultado.PRICE && <Resultado
         resultado={resultado}
         />}
